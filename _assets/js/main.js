01fdf47a194beeb5d13f94eea828677f28e17ffe
CarloBattista <carlitobatti@gmail.com>
2023-03-31 15:14:26 +0200

    codice js gioco, al momento senza bonus

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -19,10 +19,38 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 */
-const btnPlay = document.querySelector(".btn_play"); // Seleziono il bottone Play nel DOM
 
-btnPlay.addEventListener("click", function() { // Al click del bottone Play
-    for (let i = 1; i <= 100; i++) { // Parte il ciclio che genera un numero da 1 a 100
-        
-    }
+const btnPlay = document.querySelector(".btn_play"); // Seleziono il bottone Play nel DOM
+const message = document.querySelector(".container_message"); // Seleziono il messaggio nel DOM
+
+btnPlay.addEventListener("click", function() {
+
+    message.remove() // Il messaggio viene rimosso dal DOM al click del bottone
+
+    btnPlay.innerHTML = "Stai giocando!!"; // Al click del bottone viene cambiato il testo all'interno
+
+    btnPlay.classList.add("btnAnimation"); // Al click del bottone viene aggiunta una classe che gli fa fare un'animazione all'infinito
+
+    function createDiv(html, classs, text) { // Associo delle richieste alla funzione
+        let element = document.createElement(html);
+    
+        element.className = classs;
+    
+        element.innerText = text; // Inserisco text dentro al div
+    
+        return element // Da qui in poi il codice non viene più letto
+    };
+    
+    const gridList = document.querySelector(".grid_List"); // Seleziono la griglia nel DOM
+    
+    for (let i = 1; i <= 100; i++) { // Ciclo FOR per far generare un numero da 1 a 100
+        const divBox = createDiv("div", "box", i); // Collego la richiesta della funzione a una variabile
+    
+        divBox.addEventListener("click", function () { // Al click di ogni elemento BOX gli viene aggiunta una classe che gli aggiunge un Background Color
+            this.classList.toggle("clickedBlue"); // Grazie al this rendo univoco ogni click del BOX
+        })
+    
+        gridList.append(divBox) // Stampo in pagina i BOX
+    };
+
 })
