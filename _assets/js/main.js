d9153407049852fb828e3215c574acafe1303bee
CarloBattista <carlitobatti@gmail.com>
2023-04-03 12:47:07 +0200

    Possibilità di scelta del livello funzionante

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -20,14 +20,12 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 
 */
 
-// Come posso permetter di far generare massimo una griglia quando vado a cliccare il bottone, invece che ogni vola che clicco il bottone vengono generate più griglie
-
 const btnPlay = document.querySelector(".btn_play"); // Seleziono il bottone Play nel DOM
 const message = document.querySelector(".container_message"); // Seleziono il messaggio nel DOM
 
 let isGridGenerated = false; // Variabile booleana per tenere traccia se la griglia è stata già generata
 
-btnPlay.addEventListener("click", function() {
+btnPlay.addEventListener("click", function () {
 
     message.remove() // Il messaggio viene rimosso dal DOM al click del bottone
 
@@ -35,30 +33,35 @@ btnPlay.addEventListener("click", function() {
 
     btnPlay.classList.add("btnAnimation"); // Al click del bottone viene aggiunta una classe che gli fa fare un'animazione all'infinito
 
-    if(!isGridGenerated) { // Controlla se la griglia è già stata generata
+    if (!isGridGenerated) { // Controlla se la griglia è già stata generata
         isGridGenerated = true; // Variabile impostata su True per indicare che la griglia è stata generata, quindi non andrà a creare più griglie
+
+        const selectLevels = document.querySelector(".levels_Game") // Seleziono nel dom l'input select
+        let valueSelectValue = selectLevels.options[selectLevels.selectedIndex].value; // Prende gli elementi value dentro la option
+        let valueSelectText = selectLevels.options[selectLevels.selectedIndex].text; // Prende gli elementi text dentro la option
+        console.log("Hai impostato il livello su: " + valueSelectValue)
 
         function createDiv(html, classs, text) { // Associo delle richieste alla funzione
             let element = document.createElement(html);
-        
+
             element.className = classs;
-        
+
             element.innerText = text; // Inserisco text dentro al div
-        
+
             return element // Da qui in poi il codice non viene più letto
         };
-        
+
         const gridList = document.querySelector(".grid_List"); // Seleziono la griglia nel DOM
-        
-        for (let i = 1; i <= 100; i++) { // Ciclo FOR per far generare un numero da 1 a 100
+
+        for (let i = 1; i <= valueSelectValue; i++) { // Ciclo FOR per far generare un numero da 1 a 100
             const divBox = createDiv("div", "box", i); // Collego la richiesta della funzione a una variabile
-        
+
             divBox.addEventListener("click", function () { // Al click di ogni elemento BOX gli viene aggiunta una classe che gli aggiunge un Background Color
                 this.classList.toggle("clickedBlue"); // Grazie al this rendo univoco ogni click del BOX
             })
-        
+
             gridList.append(divBox) // Stampo in pagina i BOX
         };
     }
 
-})
+});
